9f14ab62cf5fa1d24851cb250943fe3e28b328bd
lauferreyra <lauferreyra10@gmail.com>
2026-08-30 23:02:12 -0300

    feat: add order created event consumer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service.js';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('order.created')
+  handleOrderCreated(@Payload() data: unknown) {
+    console.log('📩 Evento recibido: order.created');
+    console.log(data);
   }
 }
