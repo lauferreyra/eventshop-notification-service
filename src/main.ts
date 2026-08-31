25d3219a9abf7256aceb08b863205ff484ea37c3
lauferreyra <lauferreyra10@gmail.com>
2026-08-30 22:17:25 -0300

    feat: connect notification service to RabbitMQ

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  MicroserviceOptions,
+  Transport,
+} from '@nestjs/microservices';
+
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app =
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      AppModule,
+      {
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:admin@localhost:5672'],
+          queue: 'notification_queue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    );
+
+  await app.listen();
 }
-await bootstrap();
+
+bootstrap();
